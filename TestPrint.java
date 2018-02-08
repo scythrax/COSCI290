3af5f05455581f0c0c68cd81362749417c23c884
johnpauldejesus1980 <johnpauldejesus1980@hotmail.com>
2018-02-08 18:10:27 -0500

    Lab1

---
+++ TestPrint.java
@@ -0,0 +1,24 @@
+//John Paul De Jesus
+//Co Sci 290
+//Testing different print examples
+
+ /*
+   This application is a demo to test different
+   printout statements.
+ */
+
+public class TestPrint{
+  
+  //main method, where the application starts
+  public static void main(String[] args){
+ 
+    System.out.println( " __________            ___.   .__              ._.  \n"
+                      + " \\____    /____   _____\\_ |__ |__| ____   _____| |  \n"
+                      + "    /     //  _ \\ /     \\| __ \\|  |/ __ \\ /  ___/ | \n"
+                      + "   /     /(  <_> )  Y Y  \\ \\_\\ \\  \\  ___/ \\___ \\ \\| \n"
+                      + "  /_______ \\____/|__|_|  /___  /__|\\___  >____  >__   \n"
+                      + "         \\/           \\/    \\/        \\/     \\/ \\/    ");
+
+  }
+  
+}
