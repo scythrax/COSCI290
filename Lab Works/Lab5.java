cde94c01cee50166514b41a15ef1f00b0c8987bc
johnpauldejesus1980 <johnpauldejesus1980@hotmail.com>
2018-03-01 17:45:14 -0500

    Practice1

---
+++ Lab Works/Lab5.java
@@ -0,0 +1,57 @@
+/*
+  John Paul De Jesus
+  Co Sci 290
+  
+  Lab 5
+  - fix the logic and make this code check if each character is a letter
+*/
+
+import java.util.Scanner;
+public class Lab5{
+
+  public static void main(String[] args){
+    //instantiate the Scanner object
+    Scanner input = new Scanner(System.in);
+    
+    int nameLength = 0;
+    String answer = "";
+    boolean flag = false; //flag is optional
+    
+    //example of a while loop
+    while(nameLength < 7){
+      
+      System.out.println("What is your name?");
+    
+      //get answer from console
+      answer = input.next();
+      
+      //check if name is less than 2 characters
+      if(answer.length() < 7){
+      System.out.println("Please enter in a name that is at least "
+                        + "seven uppercase letters long and no numbers");
+        flag = false;
+      }
+      else{ //name is at least 7 characters
+        
+        //check if name contains letter
+        
+        //use a for-loop because we know how many letters it has
+        for(int index = 0; index < answer.length(); index++){
+          
+          System.out.println(answer.charAt(index));
+          
+          //check if the character is a letter or number and upperase
+          if(!Character.isLetter(answer.charAt(index)) == Character.isUpperCase(answer.charAt(index))){ //if it's not a letter
+            System.out.println("Please enter in a name that is at least "
+                  + "seven uppercase letters long and no numbers");      
+            break; //gets out of current loop
+          }
+        }//end of for-loop
+        
+      }
+     
+      nameLength = answer.length();
+             
+    } //end while loop
+  } //end of main method
+} //end of class
---
+++ Practice1.java
@@ -0,0 +1,42 @@
+/*
+  John Paul De Jesus
+  Co Sci 290
+  
+  Lab - comment every single line of code
+  
+  Output:
+  
+  
+  */
+import java.util.Scanner; // is a Java object that allows you take user input
+public class Practice1{ // name of the java file
+  
+  //entry point of app
+  public static void main(String[] args){ 
+    // it is a access specifier that means it will be accessed by publically. 
+    // static : it is access modifier that means when the java program is load then it will create the space in memory automatically. 
+    // void : it is a return type i.e it does not return any value. main() : it is a method or a function name
+    
+    Scanner input = new Scanner(System.in); // needs import java.util.Scanner to be able to use this statement which allows user input
+    int operand1; // Variables of type int store the actual binary value for the integer you want for operand1.
+    int operand2; // Variables of type int store the actual binary value for the integer you want to operand2.
+    
+    System.out.println("Please enter in a whole number..."); // type in a whole number for operand1
+    
+    operand1 = input.nextInt(); //stores a number in operand1
+    
+    System.out.println("Please enter in another whole number..."); // type in a whole number for operand2
+    
+    operand2 = input.nextInt(); //stores a number in operand2
+    
+    System.out.println("Let's do some basic math! \n "
+                      + "Here are the numbers you chose."); // outputs a statement
+    System.out.println("Operand1: " + operand1 + " and Operand2: " + operand2); // outputs Operand1 and Operand2 
+    System.out.println(operand1 + operand2); // adds operand1 and operand2
+    System.out.println(operand1 * operand2); // multiplies operand1 and operand2
+    System.out.println(operand1 / operand2); // divides operand1 and operand2
+    System.out.println(operand1 - operand2); // subtracts operand1 and operand2
+    
+  }
+  
+}
